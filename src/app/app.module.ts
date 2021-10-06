bf6148282fe1b5152b0771f44215895173b4908b
Henry Kinard <kinard.h@husky.neu.edu>
2021-10-06 14:20:09 -0400

    Add ledger

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GameCenterComponent } from './game-center/game-center.component';
+import { LedgerComponent } from './ledger/ledger.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GameCenterComponent
+    GameCenterComponent,
+    LedgerComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/ledger/ledger.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { faArrowUp } from '@fortawesome/free-solid-svg-icons';
+import { faArrowDown } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-ledger',
+  templateUrl: './ledger.component.html',
+  styleUrls: ['./ledger.component.css']
+})
+export class LedgerComponent implements OnInit {
+
+  constructor() { }
+
+  time = new Date().getTime()
+  faArrowUp = faArrowUp;
+  faArrowDown = faArrowDown;
+
+  transactions = [
+    {
+      transactionType: 'CardRefill',
+      dateAndTime: '2pm',
+      numberTokensPurchased: 20,
+      totalTokenCost: 5.00,
+      balanceAfterTransaction: 20
+    },
+
+    {
+      transactionType: 'GamePlay',
+      dateAndTime: '3pm',
+      gameId: 1,
+      gameTitle: 'Rock Run',
+      tokenCostPerGame: 2,
+      balanceAfterTransaction: 18
+    },
+
+    {
+      transactionType: 'GamePlay',
+      dateAndTime: '4pm',
+      gameId: 2,
+      gameTitle: 'Go Stop Go',
+      tokenCostPerGame: 4,
+      balanceAfterTransaction: 14
+    }
+  ];
+
+  ngOnInit(): void {
+  }
+
+}
